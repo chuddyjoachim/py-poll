217495322639adf107a6acf92683888d78965ec6
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2021-09-29 10:48:28 +0100

    update db model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,20 +1,26 @@
+import uuid
+
 from sqlalchemy import Integer, String, DateTime, Column
 from sqlalchemy.sql import func
 from app.config.db import Base
 
-class User(Base):
-    __tablename__ = 'users'
+def get_uuid():
+    return str(uuid.uuid4())
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, default=get_uuid())
     username = Column(String, nullable=False)
     email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now())
 
 
+class Poll(Base):
+    __tablename__ = "poll"
 
-class poll(Base):
-    __tablename__= 'poll'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(String, primary_key=True, default=get_uuid())
     polltype = Column(String, nullable=False)
